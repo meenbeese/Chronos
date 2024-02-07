f00b036e41e91df8de328a9948ec74b433fb22cf
Meenbeese <meenbeese@tutanota.com>
2024-02-07 17:28:39 -0500

    Rewrite ScrollableBottomSheetBehavior in Kotlin

---
+++ app/src/main/java/me/jfenn/alarmio/utils/ScrollableBottomSheetBehavior.kt
@@ -0,0 +1,25 @@
+package me.jfenn.alarmio.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+
+class ScrollableBottomSheetBehavior<V : View>(context: Context?, attrs: AttributeSet?) :
+    BottomSheetBehavior<V>(
+        context!!, attrs
+    ) {
+    override fun onInterceptTouchEvent(
+        parent: CoordinatorLayout,
+        child: V,
+        event: MotionEvent
+    ): Boolean {
+        super.onInterceptTouchEvent(parent, child, event)
+        return false
+    }
+}
