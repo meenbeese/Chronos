c426239d925fe4df322aedaec572b9eb0ac54fd1
Meenbeese <meenbeese@tutanota.com>
2024-02-07 17:33:19 -0500

    Rewrite ImageUtils in Kotlin

---
+++ app/src/main/java/me/jfenn/alarmio/utils/ImageUtils.kt
@@ -0,0 +1,37 @@
+package me.jfenn.alarmio.utils
+
+import android.net.Uri
+import android.widget.ImageView
+
+import com.bumptech.glide.Glide
+
+import me.jfenn.alarmio.data.PreferenceData
+
+import java.io.File
+
+
+object ImageUtils {
+    /**
+     * Gets the current user-defined background image from SharedPreferences and applies
+     * it to the passed view.
+     *
+     * @param imageView         The ImageView to apply the background image to.
+     */
+    @JvmStatic
+    fun getBackgroundImage(imageView: ImageView) {
+        val backgroundUrl = PreferenceData.BACKGROUND_IMAGE.getValue<String>(imageView.context)
+        if (backgroundUrl != null && backgroundUrl.isNotEmpty()) {
+            if (backgroundUrl.startsWith("http")) Glide.with(imageView.context).load(backgroundUrl)
+                .into(imageView) else if (backgroundUrl.contains("://")) {
+                if (backgroundUrl.startsWith("content://")) {
+                    var path = Uri.parse(backgroundUrl).lastPathSegment
+                    path = if (path != null && path.contains(":")) "/storage/" + path.replaceFirst(
+                        ":".toRegex(),
+                        "/"
+                    ) else Uri.parse(backgroundUrl).path
+                    Glide.with(imageView.context).load(File(path!!)).into(imageView)
+                } else Glide.with(imageView.context).load(Uri.parse(backgroundUrl)).into(imageView)
+            } else Glide.with(imageView.context).load(File(backgroundUrl)).into(imageView)
+        }
+    }
+}
