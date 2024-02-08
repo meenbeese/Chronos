1de4a9f7198ac57a19c1db4ba8fca36452e977e1
Meenbeese <meenbeese@tutanota.com>
2024-02-07 20:09:13 -0500

    Rewrite DebugUtils in Kotlin

---
+++ app/src/main/java/me/jfenn/alarmio/utils/DebugUtils.kt
@@ -0,0 +1,46 @@
+package me.jfenn.alarmio.utils
+
+import me.jfenn.alarmio.Alarmio
+
+import java.lang.reflect.Constructor
+import java.lang.reflect.InvocationTargetException
+
+
+object DebugUtils {
+    private val SETUP_TASKS = arrayOf(
+        "me.jfenn.alarmio.utils.LeakCanaryTask",
+        "me.jfenn.alarmio.utils.CrasherTask"
+    )
+
+    /**
+     * Set up any debug modules from the registered tasks. Should
+     * be called inside the Application class's onCreate.
+     *
+     * @param alarmio An instance of the current application class.
+     */
+    @JvmStatic
+    fun setup(alarmio: Alarmio?) {
+        for (task in SETUP_TASKS) {
+            try {
+                val constructor = Class.forName(task).getConstructor() as Constructor<SetupTask>
+                constructor.isAccessible = true
+                constructor.newInstance().setup(alarmio)
+                break
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+            } catch (e: NoSuchMethodException) {
+                e.printStackTrace()
+            } catch (e: IllegalAccessException) {
+                e.printStackTrace()
+            } catch (e: InstantiationException) {
+                e.printStackTrace()
+            } catch (e: InvocationTargetException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    interface SetupTask {
+        fun setup(alarmio: Alarmio?)
+    }
+}
