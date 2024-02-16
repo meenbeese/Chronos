b31d2d252aa20ae76339cece1b3b55569a6793a4
Meenbeese <meenbeese@tutanota.com>
2024-02-15 20:19:31 -0500

    Rewrite Subscribable in Kotlin

--- app/src/main/java/me/jfenn/alarmio/interfaces/Subscribable.kt
+++ app/src/main/java/me/jfenn/alarmio/interfaces/Subscribable.kt
@@ -1,14 +1,11 @@
-package me.jfenn.alarmio.interfaces;
+package me.jfenn.alarmio.interfaces
 
 /**
  * A basic interface to allow views, dialogs, and activities
  * and stuff to be subscribed and un-subscribed easily
  * to adhere to their respective life-cycles.
  */
-public interface Subscribable {
-
-    void subscribe();
-
-    void unsubscribe();
-
+interface Subscribable {
+    fun subscribe()
+    fun unsubscribe()
 }
