7e4d140aaf1e809405f1e6a68ad4fc8158659263
Meenbeese <meenbeese@tutanota.com>
2024-03-02 19:40:32 -0500

    Implement bitmap conversion fun in ImageUtils

--- app/src/main/java/com/meenbeese/chronos/utils/ImageUtils.kt
+++ app/src/main/java/com/meenbeese/chronos/utils/ImageUtils.kt
@@ -1,5 +1,9 @@
 package com.meenbeese.chronos.utils
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
 
@@ -11,6 +15,28 @@ import java.io.File
 
 
 object ImageUtils {
+    /**
+     * Converts drawables to bitmaps.
+     *
+     * @return A bitmap representing the drawable asset.
+     */
+    fun Drawable.toBitmap() : Bitmap {
+        (this as? BitmapDrawable)?.let {
+            if (it.bitmap != null)
+                return it.bitmap
+        }
+
+        val bitmap = if (intrinsicWidth <= 0 || intrinsicHeight <= 0)
+            Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+        else Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmap)
+        setBounds(0, 0, canvas.width, canvas.height)
+        draw(canvas)
+
+        return bitmap
+    }
+
     /**
      * Gets the current user-defined background image from SharedPreferences and applies
      * it to the passed view.
