92f5216a8e363be17d68e27b9f4b86c6e44e6d1b
Meenbeese <meenbeese@tutanota.com>
2024-07-25 21:51:03 -0400

    Bump version to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.meenbeese.chronos"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
