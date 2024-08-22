6d1c2ffcbf9addc2da9d091161e65a7a728bfc6d
Meenbeese <meenbeese@tutanota.com>
2024-08-21 20:10:06 -0400

    Update Kotlin and Gradle to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.9.24")
+    val kotlinVersion by extra("2.0.10")
     
     repositories {
         google()
