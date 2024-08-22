a239e959bb47658ee539ae102dfed6e700ce516c
Meenbeese <meenbeese@tutanota.com>
2024-08-22 12:35:29 -0400

    Bump version to 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.meenbeese.chronos"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.7.1"
+        versionCode = 10
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
