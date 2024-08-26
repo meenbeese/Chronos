aca82e6bb98b8272bd525802f36c2707e6b6c1f8
Meenbeese <meenbeese@tutanota.com>
2024-08-26 17:32:28 -0400

    Target SDK 35 and drop SDK < 26 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.meenbeese.chronos"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.meenbeese.chronos"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 26
+        targetSdk = 35
         versionCode = 10
         versionName = "1.8.0"
 
