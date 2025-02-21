aba63fc91be4718897320a4c0b121c4c1df1494c
Kuzey Bilgin <kuzeybilgin@proton.me>
2025-02-21 15:24:13 -0500

    Use newer ksp instead of kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -78,7 +78,7 @@ dependencies {
     implementation("androidx.media3:media3-ui:1.5.1")
 
     // Room DB
-    kapt("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
 
