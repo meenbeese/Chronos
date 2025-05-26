ff5c372f9337c1027ec487d594016f25fedfe67f
Kuzey Bilgin <kuzeybilgin@proton.me>
2025-05-26 19:03:55 -0400

    Start alarm activity on the main UI thread

--- app/src/main/java/com/meenbeese/chronos/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/meenbeese/chronos/receivers/AlarmReceiver.kt
@@ -3,6 +3,7 @@ package com.meenbeese.chronos.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 import com.meenbeese.chronos.activities.AlarmActivity
 import com.meenbeese.chronos.data.AlarmData
@@ -13,12 +14,19 @@ import com.meenbeese.chronos.utils.toNullable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 import java.util.Calendar
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val alarmId = intent.getIntExtra(EXTRA_ALARM_ID, 0)
+        val alarmId = intent.getIntExtra(EXTRA_ALARM_ID, -1)
+        Log.d("AlarmReceiver", "Alarm received with id: $alarmId")
+
+        if (alarmId == -1) {
+            Log.w("AlarmReceiver", "No alarm ID found in intent!")
+            return
+        }
         val db = AlarmDatabase.getDatabase(context).alarmDao()
 
         CoroutineScope(Dispatchers.IO).launch {
@@ -41,11 +49,13 @@ class AlarmReceiver : BroadcastReceiver() {
                     alarm.saveToDatabase(context)
                 }
 
-                val ringer = Intent(context, AlarmActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    putExtra(AlarmActivity.EXTRA_ALARM, alarm)
+                withContext(Dispatchers.Main) {
+                    val ringer = Intent(context, AlarmActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        putExtra(AlarmActivity.EXTRA_ALARM, alarm)
+                    }
+                    context.startActivity(ringer)
                 }
-                context.startActivity(ringer)
             }
         }
     }
