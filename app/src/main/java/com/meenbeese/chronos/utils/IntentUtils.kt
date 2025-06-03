3ccc63203b6cd8397b2443793a9b61a9f21c0355
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2025-06-03 18:28:38 +0200

    Update app/src/main/java/com/meenbeese/chronos/utils/IntentUtils.kt
    Co-authored-by: Kuzey Bilgin <kuzeybilgin@proton.me>

--- app/src/main/java/com/meenbeese/chronos/utils/IntentUtils.kt
+++ app/src/main/java/com/meenbeese/chronos/utils/IntentUtils.kt
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+
 import com.meenbeese.chronos.R
 
 fun safeStartActivity(context: Context, intent: Intent): Boolean {
