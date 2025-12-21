bf4aec6278a5185b164b443284c4531bf9048462
Kuzey Bilgin <kuzeybilgin@proton.me>
2025-12-21 17:05:05 -0500

    Update AGP and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version("8.13.0") apply(false)
-    id("com.google.devtools.ksp") version("2.2.0-2.0.2") apply(false)
-    id("org.jetbrains.kotlin.android") version("2.2.0") apply(false)
-    id("org.jetbrains.kotlin.plugin.compose") version("2.2.0") apply(false)
-    id("org.jetbrains.kotlin.plugin.parcelize") version("2.2.0") apply(false)
+    id("com.android.application") version("8.13.2") apply(false)
+    id("com.google.devtools.ksp") version("2.2.21-2.0.4") apply(false)
+    id("org.jetbrains.kotlin.android") version("2.2.21") apply(false)
+    id("org.jetbrains.kotlin.plugin.compose") version("2.2.21") apply(false)
+    id("org.jetbrains.kotlin.plugin.parcelize") version("2.2.21") apply(false)
     id("org.jetbrains.kotlin.plugin.serialization") version("1.9.0") apply(false)
 }
