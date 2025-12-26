bc8bf20d0666b4f5841de06d7ee679bf97fbd426
Kuzey Bilgin <kuzeybilgin@proton.me>
2025-12-26 18:57:46 -0500

    Update to latest dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,13 +83,13 @@ ksp {
 
 dependencies {
     // Core
-    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.activity:activity-compose:1.12.2")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
-    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 
     // Compose
     implementation("androidx.compose.ui:ui:1.9.3")
@@ -108,15 +108,15 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer-hls:1.9.0")
 
     // Room DB
-    ksp("androidx.room:room-compiler:2.8.2")
-    implementation("androidx.room:room-ktx:2.8.2")
-    implementation("androidx.room:room-runtime:2.8.2")
+    ksp("androidx.room:room-compiler:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    implementation("androidx.room:room-runtime:2.8.4")
 
     // Picker
-    implementation("com.github.skydoves:colorpicker-compose:1.1.2")
+    implementation("com.github.skydoves:colorpicker-compose:1.1.3")
 
     // Arrow
-    implementation("io.arrow-kt:arrow-core:2.1.2")
+    implementation("io.arrow-kt:arrow-core:2.2.1.1")
 
     // Image
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
