72aac86241edce1d0fa369355c20434011507d1d
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-04-10 19:40:30 -0400

    Bump version to 2026.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.meenbeese.chronos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "2026.1.0"
+        versionCode = 16
+        versionName = "2026.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
