76e7573dd8180f96abf9cbb2fba549a7b42651be
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-04-10 18:42:34 -0400

    Make the alarm import transactional

--- app/src/main/java/com/meenbeese/chronos/db/AlarmSerdes.kt
+++ app/src/main/java/com/meenbeese/chronos/db/AlarmSerdes.kt
@@ -5,24 +5,27 @@ import android.content.Context
 import arrow.core.Either
 import arrow.core.raise.either
 
+import androidx.room.withTransaction
+
 import kotlinx.serialization.json.Json
 
 class AlarmSerdes {
-    private val jsonFormatter = Json { prettyPrint = true }
+    private val jsonFormatter = Json { prettyPrint = false }
 
     suspend fun importAlarmDataFromJson(
         context: Context,
         json: String
     ): Either<Throwable, Int> = either {
         val db = AlarmDatabase.getDatabase(context)
-        val oldAlarms = db.alarmDao().getAllAlarmsDirect()
         val newAlarms = Either.catch {
             jsonFormatter.decodeFromString<List<AlarmEntity>>(json)
         }.bind()
 
-        db.alarmDao().apply {
-            oldAlarms.forEach { delete(it) }
-            newAlarms.forEach { insert(it) }
+        db.withTransaction {
+            val dao = db.alarmDao()
+            val oldAlarms = dao.getAllAlarmsDirect()
+            oldAlarms.forEach { dao.delete(it) }
+            newAlarms.forEach { dao.insert(it) }
         }
 
         newAlarms.size
